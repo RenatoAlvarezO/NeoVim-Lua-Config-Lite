e7a28d88c99050d413e8d90559babf4526ac90f5
Renato Alvarez <renatoalvarez96@gmail.com>
2022-07-10 20:45:56 -0400

    Transparent Terminal

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,5 +1,12 @@
+
 vim.cmd[[
   let g:gruvbox_contrast_dark = "hard"
+  let g:gruvbox_transparent_bg = 1
+  " let ayucolor = "mirage"
   set termguicolors
   colorscheme gruvbox
+  " colorscheme ayu
+  hi! Normal ctermbg=NONE guibg=NONE
 ]]
+
+
