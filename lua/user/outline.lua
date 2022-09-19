76b877c3b128361dd8c8988d533f00751ce90c78
Renato Alvarez <renatoalvarez96@gmail.com>
2022-09-19 09:03:57 -0400

    Outline Setup

--- lua/user/outline.lua
+++ lua/user/outline.lua
@@ -1,5 +1,17 @@
-vim.g.symbol_outline = {
+-- vim.g.symbol_outline = {
+--     position = 'right',
+--     width = 15,
+--     show_numbers = true,
+-- }
+
+local ok, outline = pcall(require, "symbols-outline");
+if not ok then
+    print('outline no cargo')
+    return
+end
+
+outline.setup({
     position = 'right',
     width = 15,
     show_numbers = true,
-}
+})
