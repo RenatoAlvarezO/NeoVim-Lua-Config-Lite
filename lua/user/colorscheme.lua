5222b35d8121961c6eab02661390dab0769f2119
Renato Alvarez <renatoalvarez96@gmail.com>
2023-05-13 19:55:51 -0400

    Color Scheme update

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -2,12 +2,15 @@
 vim.cmd[[
   set termguicolors
   let g:gruvbox_contrast_dark = "hard"
+  let g:gruvbox_bold = 1
+  let g:gruvbox_italic = 1
+  let g:gruvbox_underscore = 1
   let g:gruvbox_transparent_bg = 1
   set background=dark
   let g:everforest_enable_italic = 1
   let g:everforest_background="hard"
   let ayucolor = "mirage"
-  colorscheme everforest
+  colorscheme gruvbox 
   " hi! Normal ctermbg=NONE guibg=NONE
 ]]
 
