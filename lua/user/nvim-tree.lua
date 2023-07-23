7239a35ff3c5ac583e8f81956d13c439c3618db1
Renato Alvarez <renatoalvarez96@gmail.com>
2023-07-23 00:17:24 -0400

    nvim-tree update

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -13,15 +13,12 @@ end
 
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
+local tree_setup_ok, tree_setup = pcall(require, "nvim-tree-on-attach")
+
 nvim_tree.setup {
   disable_netrw = true,
   hijack_netrw = true,
-  open_on_setup = false,
-  ignore_ft_on_setup = {
-    "startify",
-    "dashboard",
-    "alpha",
-  },
+  on_attach = tree_setup.on_attach,
   open_on_tab = false,
   hijack_cursor = false,
   diagnostics = {
@@ -50,21 +47,6 @@ nvim_tree.setup {
     enable = true,
     ignore = true,
     timeout = 500,
-  },
-  view = {
-    width = 30,
-    hide_root_folder = false,
-    side = "left",
-    mappings = {
-      custom_only = false,
-      list = {
-        { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-        { key = "h", cb = tree_cb "close_node" },
-        { key = "v", cb = tree_cb "vsplit" },
-      },
-    },
-    number = false,
-    relativenumber = false,
   },
   trash = {
     cmd = "trash",
