b6b7f22a125b7fec8cf25049d0564ffbed5d4149
Renato Alvarez <ralvarez@actualisapbolivia.com>
2025-12-29 15:19:05 -0400

    Go iferr Snippet Added

--- lua/user/snippets.lua
+++ lua/user/snippets.lua
@@ -7,6 +7,20 @@ local snip = ls.snippet
 local text = ls.text_node
 
 ls.add_snippets(nil, {
+  go = {
+    snip(
+      {
+        trig = "iferr",
+        namr = "If Err",
+        dscr = "Go Error Handling"
+      },
+      text {
+        "if err != nil {",
+        "",
+        "}"
+      }
+    )
+  },
   python = {
     snip(
       {
