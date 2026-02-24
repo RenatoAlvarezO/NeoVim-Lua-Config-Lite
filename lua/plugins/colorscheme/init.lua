9664ba5d79ba7bb85328812421b614efc9433691
Renato Alvarez <ralvarez@actualisapbolivia.com>
2026-02-24 10:59:58 -0400

    Color Schemes Added

--- lua/plugins/colorscheme/init.lua
+++ lua/plugins/colorscheme/init.lua
@@ -1,7 +1,11 @@
 return {
 
-  { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...},
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  "sainnhe/everforest",
-  "Shatur/neovim-ayu"
+	{ "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = ... },
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	"sainnhe/everforest",
+	"Shatur/neovim-ayu",
+	"shaunsingh/nord.nvim",
+	"Mofiqul/adwaita.nvim",
+	"NTBBloodbath/doom-one.nvim",
+	"NLKNguyen/papercolor-theme",
 }
